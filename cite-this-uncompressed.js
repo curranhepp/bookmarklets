cc774d084471278b1bdf08da73073ea39173c3a7
Curran Heppenstall <curranhepp@gmail.com>
2014-09-07 13:37:40 -0700

    Quick fix.

--- cite-this-uncompressed.js
+++ cite-this-uncompressed.js
@@ -1,4 +1,4 @@
-!function(){
+(function(){
     var e = document.createElement("div");
         e.setAttribute("id", "citationbox");
         e.setAttribute("style", "font-size:12px; background: #FAFAFA; padding: 0 12px; z-index: 5500003 !important; position: fixed;");
@@ -24,4 +24,4 @@
     n += ct;
     document.body.insertBefore(e, document.body.firstChild);
     e.innerHTML = '<div id="citewrapper" style="border:0;border-radius:0px;box-shadow:0 5px 10px rgba(0,0,0,0.4);background:#FAFAFA; text-align:left; margin: 0 12px; padding: 0 5px; border-bottom: 1px solid gray; color: #000; position:fixed; left:0;right:0;"><a style="background:#EFEFEF!important;display: block;text-align: center;position: relative; line-height: 29px; height: 29px;" href="#" onclick="document.body.removeChild(document.body.firstChild);return false">remove</a>' + n + "</div>";
-}();
+})();
